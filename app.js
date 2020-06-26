dbc8be21cf83fd4dd8fe37912f3d82a224c3d604
Adek Niyi <adekniyi@gmail.com>
2020-06-26 04:35:46 -0700

    displaying the input value

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ var title = document.querySelector('#title')
 var author = document.querySelector('#author')
 var isbn = document.querySelector('#isbn')
 var form = document.querySelector('form')
+var tbody = document.querySelector('tbody')
 
 
-form.addEventListener('submit', getValue)
+form.addEventListener('submit', displayValue)
 
-function getValue(e) {
+function displayValue(e) {
     e.preventDefault()
+    getValue()
+    createAndDeleteRow()
+
+}
+
+function getValue() {
     titleValue = title.value;
     authorValue = author.value;
     isbnValue = isbn.value;
     console.log(titleValue)
     console.log(authorValue)
     console.log(isbnValue)
+}
 
+function createAndDeleteRow() {
+    const row = document.createElement('tr')
+    row.innerHTML = `
+    <td>${titleValue}</td>
+    <td>${authorValue}</td>
+    <td>${isbnValue}</td>
+    <td><a href="#" class="danger">X</td>`;
+    tbody.appendChild(row)
+    row.addEventListener('click', (e) => {
+        console.log(e.target)
+        if (e.target.classList.contains('danger')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    })
 }
